bffa81310f07d85941d08fe418ffd021721e9ac0
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-08-31 08:31:41 -0300

    Middleware de autenticacao

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authToken = req.headers.authorization;
+
+  if (!authToken) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = authToken.split(' ');
+
+  try {
+    const encoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = encoded.id;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
