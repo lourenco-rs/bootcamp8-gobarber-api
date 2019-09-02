24ee3cef4d80be006759e444203298a2e5cd7197
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-09-02 17:00:32 -0300

    Notificando novos agendamentos

---
+++ src/app/schemas/Notification.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const NotificationSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Number,
+    required: true,
+  },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model('Notification', NotificationSchema);
