cd707210d58ca7ef4d64771e2d2732d0e1bcbf62
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-09-02 09:25:48 -0300

    Listando agenda do prestador

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,6 +24,7 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/appointments', AppointmentController.store);
