1c76511d6ea16759f423608bb52e1248377198cb
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-09-01 22:08:21 -0300

    Listando agendamentos do usuario

--- src/routes.js
+++ src/routes.js
@@ -22,6 +22,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/appointments', AppointmentController.store);
