59de2aa4233464a84a71e402218044cf48cadcb3
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-09-03 09:47:23 -0300

    Cancelamento de agendamento

--- src/routes.js
+++ src/routes.js
@@ -24,11 +24,13 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
-routes.get('/appointments', AppointmentController.index);
 routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
